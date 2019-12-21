5ed8ff4cf5b6ad5768b4e33c55baa4281d5fa818
Bruno Silva <bruno3dcontato@gmail.com>
2019-12-21 13:43:54 -0300

    Create criarTabelaEstado.sql

---
+++ criarTabelaEstado.sql
@@ -0,0 +1,13 @@
+-- Criando a tabela estado!
+-- hifen hifen cria comentários!
+
+create table estados (
+    id INT UNSIGNED NOT NULL AUTO_ICREMENT,
+    nome VARCHAR(45) NOT NULL,
+    sigla VARCHAR(2) NOT NULL,
+    regiao ENUM('Norte', 'Nordeste', 'Centro-Oeste', 'Sudeste', 'Sul') NOT NULL,
+    populacao DECIMAL(5,2) NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE KEY (nome),
+    UNIQUE KEY (sigla)
+);
