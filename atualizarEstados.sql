489c3954e3c2c4fb6d84a9e27155549e397f8f83
Bruno Silva <bruno3dcontato@gmail.com>
2019-12-21 14:45:43 -0300

    Update atualizarEstados.sql

--- atualizarEstados.sql
+++ atualizarEstados.sql
@@ -2,8 +2,14 @@
 --caso for utilizar o modificador update certifique-se de que todos os comandos estao corretos
 --pois o update podera modificar a tabela inteira!!!
 update estados
-set nome = "Maranhão"
-where sigla = "MA"
+    set nome = "Maranhão"
+    where sigla = "MA"
 
+select nome from estados where sigla = 'MA'
 
-select `nome` from estados where sigla = "MA"
+--realizando multiplas atualizacoes
+update estados
+    set nome = "Paraná", populacao = 11.32
+    where sigla = "PR";
+
+select nome, sigla, populacao from estados where sigla = "PR";
